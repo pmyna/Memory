0309aa333a61d46c40f157c87b0bb34bdf6a46d2
Mina Hova <miname630@gmail.com>
2023-11-24 20:07:59 +0100

    playable version 1.0

--- main.py
+++ main.py
@@ -30,9 +30,11 @@ counter = 5
 timer_event = pg.USEREVENT + 1
 pg.time.set_timer(timer_event, 1000)
 
-# Background Image
+# Background Images
 bg_image = pg.image.load('Images/background.jpg')
 screen.blit(pg.transform.scale(bg_image, (screenWidth, screenHeight)), (0, 0))
+correct_img = pg.image.load('Images/accept.png')
+correct_img = pg.transform.scale(correct_img, (card_size-padding, card_size-padding))
 
 
 # Memory Images
@@ -42,13 +44,23 @@ for item in os.listdir('./Images/cards'):
     icon = pg.transform.scale(icon, (card_size-padding, card_size-padding))
     memory_images.append(icon)
 
+# Coordinates
+coordinates = []
+for i in range(columns):
+    for j in range(rows):
+        x_coord = i * 200 + lrMargin
+        y_coord = j * 130 + tbMargin
+        coordinates.append((x_coord, y_coord))
+
 
 class Card:
-    def __init__(self, image, x, y):
+    def __init__(self, image, x, y, cv_letter):
         self.image = image
         self.x = x
         self.y = y
         self.rect = pg.Rect(self.x, self.y, card_size, card_size)
+        self.cv_letter = cv_letter
+        self.hidden = True
 
     def draw(self):
         pg.draw.rect(screen, white, self.rect)
@@ -58,19 +70,30 @@ class Card:
     def hide(self):
         pg.draw.rect(screen, white, self.rect)
         pg.draw.rect(screen, dark_blue, self.rect, 1)
+        cover_letter = font.render(self.cv_letter, True, black)
+        screen.blit(cover_letter, (self.x + 35, self.y + 35))
+
+    def compare(self, solution):
+        if solution.image == self.image:
+            self.image = correct_img
+            self.hidden = False
+            if len(targets) > 1:
+                del targets[0]
+            else:
+                print("Game Over")
+        else:
+            self.draw()
 
 
-coordinates = []
-for i in range(columns):
-    for j in range(rows):
-        x_coord = i * 200 + lrMargin
-        y_coord = j * 130 + tbMargin
-        coordinates.append((x_coord, y_coord))
-
+# Build Deck
 playing_cards = []
+cover = list(string.ascii_uppercase)
 for i in range(len(memory_images)):
-    card = Card(memory_images[i], coordinates[i][0], coordinates[i][1])
+    card = Card(memory_images[i], coordinates[i][0], coordinates[i][1], cover[i])
     playing_cards.append(card)
+
+targets = random.sample(range(len(playing_cards)), (columns*rows))
+print(targets)
 
 
 def menubar():
@@ -80,54 +103,48 @@ def menubar():
 
 
 def cards(mode):
-    cv_index = 0
-    cover = list(string.ascii_uppercase)
     for tile in playing_cards:
-        if mode == 'init' or 'reveal':
+        if mode == 'init':
             tile.draw()
-        if mode == 'cover':
+        if mode == 'cover' and tile.hidden:
             tile.hide()
-            cover_letter = font.render(cover[cv_index], True, black)
-            screen.blit(cover_letter, (tile.x + 35, tile.y + 35))
-        cv_index += 1
 
 
-def display_timer():
-    global counter
-    global timer
-
-    pg.draw.rect(screen, white, [50, 50, 100, 80])
-    pg.draw.rect(screen, dark_blue, [50, 50, 100, 80], 1)
-    timer = font.render(str(counter), True, dark_blue)
-    screen.blit(timer, (75, 75))
+def check(mouse_click):
+    for tile in playing_cards:
+        if tile.rect.collidepoint(mouse_click):
+            tile.draw()
+            tile.compare(playing_cards[targets[0]])
 
 
-answer = random.randint(0, len(memory_images) - 1)
+button = pg.Rect(300, 70, 100, 80)
+pg.draw.rect(screen, white, button)
+button_title = font.render('HIDE', True, dark_blue)
+screen.blit(button_title, (310, 80))
+hide = False
+tile_clicked = False
 
 while playing:
     pg.display.update()
     menubar()
-    display_timer()
-
+    cards('init')
     # Input Events
     for event in pg.event.get():
         if event.type == pg.QUIT:
             playing = False
-        if event.type == timer_event:
-            if counter > 0:
-                cards('init')
-                pg.draw.rect(screen, white, [50, 50, 100, 80])
-                pg.draw.rect(screen, dark_blue, [50, 50, 100, 80], 1)
-                timer = font.render(str(counter), True, black)
-                counter -= 1
-            else:
-                cards('cover')
-                pg.draw.rect(screen, white, [300, 50, card_size, card_size])
-                pg.draw.rect(screen, dark_blue, [300, 50, card_size, card_size], 1)
-                screen.blit(memory_images[answer], (305, 55))
         if event.type == pg.MOUSEBUTTONDOWN:
-            print(pg.mouse.get_pos())
-
+            position = pg.mouse.get_pos()
+            if button.collidepoint(position):
+                hide = True
+            else:
+                tile_clicked = True
+    if hide:
+        cards('cover')
+        pg.draw.rect(screen, white, [300, 50, card_size, card_size])
+        pg.draw.rect(screen, dark_blue, [300, 50, card_size, card_size], 1)
+        screen.blit(playing_cards[targets[0]].image, (305, 55))
+    if tile_clicked:
+        check(position)
     pg.display.update()
 
 pg.quit()
