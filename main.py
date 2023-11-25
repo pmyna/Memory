e920318c3c9dcd9e52e4938c0ae8023306152358
Mina Hova <miname630@gmail.com>
2023-11-25 11:16:15 +0100

    playable version 1.1. [MVP]

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import os
 import random
 import string
+import time
+
 import pygame as pg
 
 pg.init()
+
+# Init Round
+columns = 2 # <---------------- 3 / 4
+rows = 2 # <------------------- 3
+game = 'trial'
+game_title = "Trial-Round"
 
 # Game Constants
 screenWidth = 1280
 screenHeight = 700
 card_size = 110
 playing = True
-columns, rows, padding = 3, 2, 10
+padding = 10
 lrMargin = round((screenWidth - ((card_size + padding) * columns)) // 1.5)
 tbMargin = round((screenHeight - ((card_size + padding) * rows)) // 5)
 
@@ -19,28 +27,24 @@ dark_blue = (34, 62, 99)
 white = (255, 255, 255)
 black = (0, 0, 0)
 
-
 # Game Screen
 screen = pg.display.set_mode((screenWidth, screenHeight), pg.RESIZABLE)
 pg.display.set_caption('Labor UE 2')
 font = pg.font.SysFont('Work Sans', 60)
 
-# Timers
-counter = 5
-timer_event = pg.USEREVENT + 1
-pg.time.set_timer(timer_event, 1000)
-
 # Background Images
 bg_image = pg.image.load('Images/background.jpg')
 screen.blit(pg.transform.scale(bg_image, (screenWidth, screenHeight)), (0, 0))
+screen.fill(white)
 correct_img = pg.image.load('Images/accept.png')
 correct_img = pg.transform.scale(correct_img, (card_size-padding, card_size-padding))
-
+false_img = pg.image.load('Images/cancel.png')
+false_img = pg.transform.scale(false_img, (card_size-padding, card_size-padding))
 
 # Memory Images
 memory_images = []
-for item in os.listdir('./Images/cards'):
-    icon = pg.image.load(f'./Images/cards/{item}')
+for item in os.listdir(f'./Images/{game}'):
+    icon = pg.image.load(f'./Images/{game}/{item}')
     icon = pg.transform.scale(icon, (card_size-padding, card_size-padding))
     memory_images.append(icon)
 
@@ -52,6 +56,14 @@ for i in range(columns):
         y_coord = j * 130 + tbMargin
         coordinates.append((x_coord, y_coord))
 
+# Hide Button
+hide_button = pg.Rect(100, 70, 150, 70)
+pg.draw.rect(screen, dark_blue, hide_button)
+pg.draw.rect(screen, white, hide_button, 1)
+button_title = font.render('HIDE', True, white)
+screen.blit(button_title, (125, 88))
+hide = False
+tile_clicked = False
 
 class Card:
     def __init__(self, image, x, y, cv_letter):
@@ -60,7 +72,7 @@ class Card:
         self.y = y
         self.rect = pg.Rect(self.x, self.y, card_size, card_size)
         self.cv_letter = cv_letter
-        self.hidden = True
+        self.show = True
 
     def draw(self):
         pg.draw.rect(screen, white, self.rect)
@@ -76,13 +88,14 @@ class Card:
     def compare(self, solution):
         if solution.image == self.image:
             self.image = correct_img
-            self.hidden = False
+            self.show = False
             if len(targets) > 1:
                 del targets[0]
-            else:
-                print("Game Over")
+        elif self.image == correct_img:
+            self.image = correct_img
+            self.show = False
         else:
-            self.draw()
+            screen.blit(false_img, (self.x + 5, self.y + 5))
 
 
 # Build Deck
@@ -93,36 +106,27 @@ for i in range(len(memory_images)):
     playing_cards.append(card)
 
 targets = random.sample(range(len(playing_cards)), (columns*rows))
-print(targets)
 
 
 def menubar():
     pg.draw.rect(screen, dark_blue, [0, screenHeight - 80, screenWidth, 80])
-    title = font.render('- Round 1 -', True, white)
-    screen.blit(title, (600, screenHeight-55))
+    title = font.render(game_title, True, white)
+    screen.blit(title, (500, screenHeight-55))
 
 
 def cards(mode):
     for tile in playing_cards:
-        if mode == 'init':
-            tile.draw()
-        if mode == 'cover' and tile.hidden:
+        if mode == 'cover' and tile.show:
             tile.hide()
+        else:
+            tile.draw()
 
 
 def check(mouse_click):
     for tile in playing_cards:
         if tile.rect.collidepoint(mouse_click):
-            tile.draw()
             tile.compare(playing_cards[targets[0]])
 
-
-button = pg.Rect(300, 70, 100, 80)
-pg.draw.rect(screen, white, button)
-button_title = font.render('HIDE', True, dark_blue)
-screen.blit(button_title, (310, 80))
-hide = False
-tile_clicked = False
 
 while playing:
     pg.display.update()
@@ -134,7 +138,7 @@ while playing:
             playing = False
         if event.type == pg.MOUSEBUTTONDOWN:
             position = pg.mouse.get_pos()
-            if button.collidepoint(position):
+            if hide_button.collidepoint(position):
                 hide = True
             else:
                 tile_clicked = True
